efe59ede8896efe3d024b369601283c9acad1cc0
Islom Jumaniyozov <islom@iman.uz>
2026-01-30 11:17:04 +0500

    Remove generated reports and progress files

--- create_chapters_19_22.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Create all comprehensive content for chapters 19-22
-
-# This will be a large multi-file generation script
-# Due to token limits, I'll create the most critical files first
-
-print("Script ready to generate comprehensive content for chapters 19-22")
-print("Files will be created in their respective directories")
